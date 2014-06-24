6a6d5de2584c82b3f71f33c0595b37dde613be50
Andre Araujo <asdaraujo@gmail.com>
2014-06-24 10:44:53 -0400

    olr implementation

--- src/main/java/com/asdaraujo/ClaimClassifier.java
+++ src/main/java/com/asdaraujo/ClaimClassifier.java
@@ -37,9 +37,12 @@ import org.apache.mahout.math.DenseVector;
 import org.apache.mahout.math.RandomAccessSparseVector;
 import org.apache.mahout.math.Vector;
 import org.apache.mahout.vectorizer.encoders.ConstantValueEncoder;
+import org.apache.mahout.vectorizer.encoders.ContinuousValueEncoder;
 import org.apache.mahout.vectorizer.encoders.FeatureVectorEncoder;
 import org.apache.mahout.vectorizer.encoders.StaticWordValueEncoder;
 import org.apache.mahout.vectorizer.encoders.Dictionary;
+import org.apache.commons.lang3.tuple.Pair;
+import org.apache.commons.lang3.tuple.ImmutablePair;
 
 import com.google.common.base.Charsets;
 import com.google.common.base.Splitter;
@@ -52,6 +55,54 @@ public class ClaimClassifier {
     private static final int FEATURES = 10000;
     private static Multiset<String> overallCounts;
 
+    private Map<String, Set<Integer>> traceDictionary;
+    private FeatureVectorEncoder biasEnc;
+    private FeatureVectorEncoder ageEnc;
+    private FeatureVectorEncoder genderEnc;
+    private FeatureVectorEncoder incomeEnc;
+    private FeatureVectorEncoder inpatientEnc;
+    private FeatureVectorEncoder outpatientEnc;
+    private FeatureVectorEncoder claimEnc;
+    private List<Pair<Integer,Vector>> vectors;
+    private OnlineLogisticRegression learningAlgorithm;
+
+    public ClaimClassifier() {
+        this.traceDictionary = new TreeMap<String, Set<Integer>>();
+        this.biasEnc = new ConstantValueEncoder("bias");
+        this.biasEnc.setTraceDictionary(this.traceDictionary);
+        this.ageEnc = new StaticWordValueEncoder("age");
+        this.ageEnc.setTraceDictionary(this.traceDictionary);
+        this.genderEnc = new StaticWordValueEncoder("gender");
+        this.genderEnc.setTraceDictionary(this.traceDictionary);
+        this.incomeEnc = new StaticWordValueEncoder("income");
+        this.incomeEnc.setTraceDictionary(this.traceDictionary);
+        this.inpatientEnc = new ConstantValueEncoder("inpatient");
+        this.inpatientEnc.setTraceDictionary(this.traceDictionary);
+        this.outpatientEnc = new ConstantValueEncoder("outpatient");
+        this.outpatientEnc.setTraceDictionary(this.traceDictionary);
+        this.claimEnc = new StaticWordValueEncoder("claim");
+        this.claimEnc.setProbes(2);
+        this.claimEnc.setTraceDictionary(this.traceDictionary);
+
+        this.learningAlgorithm =
+            new OnlineLogisticRegression(
+                2, FEATURES, new L1())
+                .alpha(1).stepOffset(1000)
+                .decayExponent(0.9)
+                .lambda(3.0e-5)
+                .learningRate(20);
+
+        this.vectors = new ArrayList<Pair<Integer,Vector>>();
+    }
+
+    public void run(String inputFile) throws IOException {
+        Configuration conf = new Configuration();
+        FileSystem fs = FileSystem.get(conf);
+
+        readPoints(inputFile, conf);
+        train();
+    }
+
     private static String getAsString(BytesRefWritable ref) {
         int start = ref.getStart();
         int length = ref.getLength();
@@ -62,112 +113,103 @@ public class ClaimClassifier {
         }
     }
 
-    public static List<Vector> readPoints(String inputFile, Configuration conf) throws IOException {
+    private static double getAsDouble(BytesRefWritable ref) {
+        return Double.valueOf(getAsString(ref));
+    }
+
+    public void readPoints(String inputFile, Configuration conf) throws IOException {
+        System.out.println("Reading points from: " + inputFile);
+
         List<Vector> points = new ArrayList<Vector>();
         FileSystem fs = FileSystem.get(conf);
         Path inputPath = new Path(inputFile);
-        RCFile.Reader reader = new RCFile.Reader(fs, inputPath, conf);
+        RCFile.Reader reader = new RCFile.Reader(fs, inputPath, 104857600, conf, 0, fs.getFileStatus(inputPath).getLen());
 
+        Dictionary keys = new Dictionary();
         LongWritable rows = new LongWritable();
-        BytesWritable key = new BytesWritable();
         BytesRefArrayWritable row = new BytesRefArrayWritable();
-        Text value = new Text();
-        Hashtable<String, Integer> keys = new Hashtable<String, Integer>();
         int idx = 3;
         while (reader.next(rows)) {
+            if (rows.get() % 1000 == 0)
+                System.out.println(String.format("%d records read", rows.get()));
             reader.getCurrentRow(row);
             String id = getAsString(row.get(0));
             String review = getAsString(row.get(1));
             String age = getAsString(row.get(2));
             String gender = getAsString(row.get(3));
             String income = getAsString(row.get(4));
-            String typeI = getAsString(row.get(5));
-            String typeO = getAsString(row.get(6));
-            String claims = getAsString(row.get(7));
-            System.out.println(String.format("%s:%s:%s:%s:%s:%s:%s:%s", id, review, age, gender, income, typeI, typeO, claims));
-//
-////            String code = fields[0]+fields[1];
-////            if (!keys.containsKey(code))
-////                keys.put(code, idx++);
-////            String provider_location = "l"+fields[3]+","+fields[4];
-////            if (!keys.containsKey(provider_location))
-////                keys.put(provider_location, ++idx);
-////            String provider_zipcode = "z"+fields[5];
-////            if (!keys.containsKey(provider_zipcode))
-////                keys.put(provider_zipcode, ++idx);
-////            String referral_region = "r"+fields[6];
-////            if (!keys.containsKey(referral_region))
-////                keys.put(referral_region, ++idx);
+            double typeI = getAsDouble(row.get(5));
+            double typeO = getAsDouble(row.get(6));
+            String[] claims = getAsString(row.get(7)).split(",");
+
+            //System.out.println(String.format("%s:%s:%s:%s:%s:%f:%f:%s", id, review, age, gender, income, typeI, typeO, claims.toString()));
+
+            Vector v = new RandomAccessSparseVector(FEATURES);
+            biasEnc.addToVector((String)null, 1, v);
+            ageEnc.addToVector(age, 1, v);
+            genderEnc.addToVector(gender, 1, v);
+            incomeEnc.addToVector(income, 1, v);
+            inpatientEnc.addToVector((String)null, typeI, v);
+            outpatientEnc.addToVector((String)null, typeO, v);
+            for(int i = 0; i < claims.length; i += 2) {
+                claimEnc.addToVector(claims[i], Double.valueOf(claims[i+1]), v);
+            }
+
+            this.vectors.add(new ImmutablePair(keys.intern(review),v));
         }
         reader.close();
-//        int totalLength = idx;
-//        reader = new RCFile.Reader(fs, inputPath, conf);
-//        Hashtable<String, Vector> vecs = new Hashtable<String, Vector>();
-//        while (reader.next(key, value)) {
-//            String[] fields = value.toString().split(Character.toString((char)0x01));
-//            String provider_id = fields[2];
-//            String cnt = fields[7];
-//            String claim_total = fields[8];
-//            String paid_total = fields[9];
-//
-//            String code = fields[0]+fields[1];
-//            String provider_location = "l"+fields[3]+","+fields[4];
-//            String provider_zipcode = "z"+fields[5];
-//            String referral_region = "r"+fields[6];
-//
-//            Vector vec;
-//            if (vecs.containsKey(provider_id)) {
-//                vec = vecs.get(provider_id);
-//            } else {
-//                vec = new RandomAccessSparseVector(totalLength);
-//                vecs.put(provider_id, vec);
-//                points.add(vec);
-//                vec.setQuick(0, Double.valueOf(cnt));
-//                vec.setQuick(1, Double.valueOf(claim_total));
-//                vec.setQuick(2, Double.valueOf(paid_total));
-//            }
-//            vec.setQuick(keys.get(code), 1.0);
-//            vec.setQuick(keys.get(provider_location), 1.0);
-//            vec.setQuick(keys.get(provider_zipcode), 1.0);
-//            vec.setQuick(keys.get(referral_region), 1.0);
-//        }
-//        reader.close();
-//        return points;
-        return null;
+    }
+
+    public void train() {
+        Collections.shuffle(this.vectors);
+        System.out.printf("%d training samples\n", this.vectors.size());
+
+        double averageLL = 0.0;
+        double averageCorrect = 0.0;
+        double averageLineCount = 0.0;
+        int k = 0;
+        double step = 0.0;
+        int[] bumps = new int[]{1, 2, 5};
+        double lineCount = 0;
+
+        for (Pair<Integer,Vector> pair : this.vectors) {
+            int actual = pair.getKey();
+            Vector v = pair.getValue();
+
+            double mu = Math.min(k + 1, 200);
+            double ll = learningAlgorithm.logLikelihood(actual, v);
+            averageLL = averageLL + (ll - averageLL) / mu;
+
+            Vector p = new DenseVector(2);
+            learningAlgorithm.classifyFull(p, v);
+            int estimated = p.maxValueIndex();
+
+            int correct = (estimated == actual? 1 : 0);
+            averageCorrect = averageCorrect + (correct - averageCorrect) / mu;
+
+            learningAlgorithm.train(actual, v);
+            k++;
+            int bump = bumps[(int) Math.floor(step) % bumps.length];
+            int scale = (int) Math.pow(10, Math.floor(step / bumps.length));
+            if (k % (bump * scale) == 0) {
+                step += 0.25;
+                System.out.printf("%10d %10.3f %10.3f %10.2f %d\n",
+                        k, ll, averageLL, averageCorrect * 100, 
+                        estimated);
+            }
+            learningAlgorithm.close();
+        }
+
     }
 
     public static void main(String[] args) throws IOException {
         String inputFile = args[0];
-        System.out.println("Reading points from: " + inputFile);
+        ClaimClassifier cc = new ClaimClassifier();
+        cc.run(inputFile);
 
-        Configuration conf = new Configuration();
-        FileSystem fs = FileSystem.get(conf);
-
-        List<Vector> vectors = readPoints(inputFile, conf);
         System.exit(0);
 
-//        File base = new File(args[0]);
 //        overallCounts = HashMultiset.create();
-//
-//        Map<String, Set<Integer>> traceDictionary = new TreeMap<String, Set<Integer>>();
-//        FeatureVectorEncoder encoder = new StaticWordValueEncoder("body");
-//        encoder.setProbes(2);
-//        encoder.setTraceDictionary(traceDictionary);
-//        FeatureVectorEncoder bias = new ConstantValueEncoder("Intercept");
-//        bias.setTraceDictionary(traceDictionary);
-//        FeatureVectorEncoder lines = new ConstantValueEncoder("Lines");
-//        lines.setTraceDictionary(traceDictionary);
-//        FeatureVectorEncoder logLines = new ConstantValueEncoder("LogLines");
-//        logLines.setTraceDictionary(traceDictionary);
-//        Dictionary newsGroups = new Dictionary();
-//
-//        OnlineLogisticRegression learningAlgorithm =
-//                new OnlineLogisticRegression(
-//                        20, FEATURES, new L1())
-//                        .alpha(1).stepOffset(1000)
-//                        .decayExponent(0.9)
-//                        .lambda(3.0e-5)
-//                        .learningRate(20);
 //
 //        List<File> files = new ArrayList<File>();
 //        for (File newsgroup : base.listFiles()) {
