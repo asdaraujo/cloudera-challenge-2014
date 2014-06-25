80bcf57b6aa2775fdfc2c737fd937e8a0a348fc6
Andre Araujo <asdaraujo@gmail.com>
2014-06-25 10:30:38 -0400

    import housekeeping

--- src/main/java/com/asdaraujo/ClaimClassifier.java
+++ src/main/java/com/asdaraujo/ClaimClassifier.java
@@ -1,13 +1,11 @@
 package com.asdaraujo;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
+import com.google.common.base.Charsets;
+import com.google.common.collect.Multiset;
+
 import java.io.IOException;
 import java.io.Reader;
-import java.io.StringReader;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.List;
@@ -15,22 +13,18 @@ import java.util.Map;
 import java.util.Set;
 import java.util.TreeMap;
 
-import org.apache.hadoop.io.BytesWritable;
-import org.apache.hadoop.io.LongWritable;
+import org.apache.commons.lang3.tuple.ImmutablePair;
+import org.apache.commons.lang3.tuple.Pair;
 import org.apache.hadoop.conf.Configuration;
 import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
-import org.apache.hadoop.io.Text;
-import java.util.Hashtable;
-
 import org.apache.hadoop.hive.ql.io.RCFile;
 import org.apache.hadoop.hive.serde2.columnar.BytesRefArrayWritable;
 import org.apache.hadoop.hive.serde2.columnar.BytesRefWritable;
+import org.apache.hadoop.io.LongWritable;
 import org.apache.lucene.analysis.Analyzer;
-import org.apache.lucene.analysis.TokenStream;
-import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.analysis.tokenattributes.CharTermAttribute;
-import org.apache.lucene.util.Version;
+import org.apache.lucene.analysis.TokenStream;
 import org.apache.mahout.classifier.sgd.L1;
 import org.apache.mahout.classifier.sgd.OnlineLogisticRegression;
 import org.apache.mahout.math.DenseVector;
@@ -38,23 +32,12 @@ import org.apache.mahout.math.Matrix;
 import org.apache.mahout.math.RandomAccessSparseVector;
 import org.apache.mahout.math.Vector;
 import org.apache.mahout.vectorizer.encoders.ConstantValueEncoder;
-import org.apache.mahout.vectorizer.encoders.ContinuousValueEncoder;
+import org.apache.mahout.vectorizer.encoders.Dictionary;
 import org.apache.mahout.vectorizer.encoders.FeatureVectorEncoder;
 import org.apache.mahout.vectorizer.encoders.StaticWordValueEncoder;
-import org.apache.mahout.vectorizer.encoders.Dictionary;
-import org.apache.commons.lang3.tuple.Pair;
-import org.apache.commons.lang3.tuple.ImmutablePair;
-
-import com.google.common.base.Charsets;
-import com.google.common.base.Splitter;
-import com.google.common.collect.ConcurrentHashMultiset;
-import com.google.common.collect.HashMultiset;
-import com.google.common.collect.Iterables;
-import com.google.common.collect.Multiset;
 
 public class ClaimClassifier {
     private static final int FEATURES = 10000;
-    private static Multiset<String> overallCounts;
 
     private Map<String, Set<Integer>> traceDictionary;
     private FeatureVectorEncoder biasEnc;
@@ -223,99 +206,6 @@ public class ClaimClassifier {
         String inputFile = args[0];
         ClaimClassifier cc = new ClaimClassifier();
         cc.run(inputFile);
-
-        System.exit(0);
-
-//        overallCounts = HashMultiset.create();
-//
-//        List<File> files = new ArrayList<File>();
-//        for (File newsgroup : base.listFiles()) {
-//            newsGroups.intern(newsgroup.getName());
-//            files.addAll(Arrays.asList(newsgroup.listFiles()));
-//        }
-//
-//        Collections.shuffle(files);
-//        System.out.printf("%d training files\n", files.size());
-//
-//        double averageLL = 0.0;
-//        double averageCorrect = 0.0;
-//        double averageLineCount = 0.0;
-//        int k = 0;
-//        double step = 0.0;
-//        int[] bumps = new int[]{1, 2, 5};
-//        double lineCount = 0;
-//
-//        Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_31);
-//
-//        Splitter onColon = Splitter.on(":").trimResults();
-//
-//        for (File file : files) {
-//
-//            BufferedReader reader = new BufferedReader(new FileReader(file));
-//            String ng = file.getParentFile().getName();
-//            int actual = newsGroups.intern(ng);
-//            Multiset<String> words = ConcurrentHashMultiset.create();
-//
-//            String line = reader.readLine();
-//            while (line != null && line.length() > 0) {
-//                if (line.startsWith("Lines:")) {
-//                    String count = Iterables.get(onColon.split(line), 1);
-//                    try {
-//                        lineCount = Integer.parseInt(count);
-//                        averageLineCount += (lineCount - averageLineCount)
-//                                / Math.min(k + 1, 1000);
-//                    } catch (NumberFormatException e) {
-//                        lineCount = averageLineCount;
-//                    }
-//                }
-//                boolean countHeader = (
-//                        line.startsWith("From:") || line.startsWith("Subject:")||
-//                                line.startsWith("Keywords:")|| line.startsWith("Summary:"));
-//                do {
-//                    StringReader in = new StringReader(line);
-//                    if (countHeader) {
-//                        countWords(analyzer, words, in);
-//                    }
-//                    line = reader.readLine();
-//                } while (line.startsWith(" "));
-//            }
-//            countWords(analyzer, words, reader);
-//            reader.close();
-//
-//
-//            Vector v = new RandomAccessSparseVector(FEATURES);
-//            bias.addToVector((String)null, 1, v);
-//            lines.addToVector((String)null, lineCount / 30, v);
-//            logLines.addToVector((String)null, Math.log(lineCount + 1), v);
-//            for (String word : words.elementSet()) {
-//                encoder.addToVector(word, Math.log(1 + words.count(word)), v);
-//            }
-//
-//
-//            double mu = Math.min(k + 1, 200);
-//            double ll = learningAlgorithm.logLikelihood(actual, v);
-//            averageLL = averageLL + (ll - averageLL) / mu;
-//
-//            Vector p = new DenseVector(20);
-//            learningAlgorithm.classifyFull(p, v);
-//            int estimated = p.maxValueIndex();
-//
-//            int correct = (estimated == actual? 1 : 0);
-//            averageCorrect = averageCorrect + (correct - averageCorrect) / mu;
-//
-//            learningAlgorithm.train(actual, v);
-//            k++;
-//            int bump = bumps[(int) Math.floor(step) % bumps.length];
-//            int scale = (int) Math.pow(10, Math.floor(step / bumps.length));
-//            if (k % (bump * scale) == 0) {
-//                step += 0.25;
-//                System.out.printf("%10d %10.3f %10.3f %10.2f %s %s\n",
-//                        k, ll, averageLL, averageCorrect * 100, ng,
-//                        newsGroups.values().get(estimated));
-//            }
-//            learningAlgorithm.close();
-//        }
-//
     }
 
     private static void countWords(Analyzer analyzer, Collection<String> words, Reader in) throws IOException {
@@ -325,7 +215,6 @@ public class ClaimClassifier {
             String s = ts.getAttribute(CharTermAttribute.class).toString();
             words.add(s);
         }
-            /*overallCounts.addAll(words);*/
     }
 }
 
